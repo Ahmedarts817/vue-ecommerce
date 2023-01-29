b6e19b4dd96724ba2515c133553b5a61b509b93b
ahmedarts817 <ahmedarts817@gmail.com>
2023-01-29 09:55:56 +0200

    neew

--- src/main.js
+++
@@ -1,9 +0,0 @@
-import { createApp } from 'vue';
-import App from './App.vue';
-import router from './router';
-import store from './store';
-import './scss/main.scss';
-import 'normalize.css';
-import 'bootstrap/dist/css/bootstrap.css';
-
-createApp(App).use(store).use(router).mount('#app');
